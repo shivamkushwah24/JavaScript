64e2189d95fcf2c654e84f244015a79211f44733
shivam kushwah <169129070+shivamkushwah24@users.noreply.github.com>
2024-07-17 09:48:46 +0000

    string

---
+++ 01_basic/05_strings.js
@@ -0,0 +1,31 @@
+  const name = "shivam"
+  const repoCount = 100
+
+  //console.log(name + repoCount + " value");
+
+  console.log(`Hello may name is ${name} and my repo is ${repoCount}`);
+
+  const gameName = new String('hitesh-hc')
+
+  console.log(gameName[0]);
+  console.log(gameName.__proto__);
+
+
+  // console.log(gameName.length);
+  // console.log(gameName.toUpperCase());
+  // console.log(gameName.charAt(2));
+  // console.log(gameName.indexOf('t'));
+
+  // const newString = gameName.substring(0,4)
+  // console.log(newString);
+
+  // const antherString = gameName.slice(-8,4)
+  // console.log(antherString);
+
+  const newStringOne = "  shivam  "
+  console.log(newStringOne);
+  console.log(newStringOne.trim());
+
+  const url = "https://shivam.com/shivam%24kushwah"
+  
+  console.log(url.replace('%24','_'));
