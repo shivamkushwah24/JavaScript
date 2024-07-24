59f41b671eaa4a82d66f5d702fe7ada675dc7f63
shivam kushwah <169129070+shivamkushwah24@users.noreply.github.com>
2024-07-24 17:45:02 +0000

    object1

--- 02_basic/04_Object1.js
+++ 02_basic/04_Object1.js
@@ -25,21 +25,26 @@ const obj2 = {3: "a",4: "b"}
 //const obj3 = {obj1,obj2}
 // const obj3 = Object.assign({},obj1,obj2)
 // console.log(obj3);
-const obj3= {...obj1, ...obj2}
-console.log(obj3);
+// 
 
-const users = [
-    {
-        id: 1,
-        email: "shivam@gmail.com"
-    }
-]
+//console.log(Object.entries(tinderUser));
 
-users[1].email
-console.log(tinderUser);
+//console.log(tinderUser.hasOwnProperty('isLoggedIn'));
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+const course = {
+    coursename: "javaScript",
+     price: "999",
+     courseInstrucor:"hitesh"
+}
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+//course.courseInstrucor
+
+const {courseInstrucor} = course
+
+console.log(courseInstrucor);
+
+//{
+    //"name": "shivam",
+    //"coursename": "js",
+  //  "price": "free"
+//}
