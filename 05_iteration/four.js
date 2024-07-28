51ce0911a7b07c92515db9c96eadcd55485e3378
shivam kushwah <169129070+shivamkushwah24@users.noreply.github.com>
2024-07-28 11:45:15 +0000

    for in loop

---
+++ 05_iteration/four.js
@@ -0,0 +1,13 @@
+const Object = {
+    js: 'javascript'
+}
+
+for (const key in Object) {
+  //  console.log(`${key} shorcut is for ${Object[key]}`);
+}
+
+const programming = ["js","rb","py","java","cpp"]
+
+for (const key in programming) {
+    console.log(programming[key]);
+}
